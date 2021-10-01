6a5e569579723953493793c5e70856b74e7746f5
Ashton Fei <yunjia.fei@gmail.com>
2021-10-01 14:48:15 +0800

    add gulp-jsdoc3

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 const { src, dest, series } = require('gulp')
 const expose = require('gulp-expose')
 const rename = require('gulp-rename')
+const jsdoc = require('gulp-jsdoc3')
 const del = require("del")
 
 const IDENTIFIER = "DocPro"
@@ -16,4 +17,11 @@ const clean = function () {
     return del(['dist/*'])
 }
 
-exports.default = series(clean, build)
+const doc = function () {
+    // const config = require('./jsdoc.json')
+    return src(["README.md", "./src/**/*.js"], { read: false })
+        .pipe(jsdoc())
+}
+
+exports.build = series(clean, build)
+exports.doc = doc
